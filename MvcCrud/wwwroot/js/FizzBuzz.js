2a3cb9588eec332ceeb4014c7e35bd99e3253503
NJS <24832578+codenjs@users.noreply.github.com>
2019-07-20 17:29:51 -0400

    Add sample javascript unit test

---
+++ MvcCrud/wwwroot/js/FizzBuzz.js
@@ -0,0 +1,32 @@
+﻿var FizzBuzz = (function () {
+    var _print = function (input) {
+        var result = '';
+
+        for (var i = 1; i <= input; i++)
+        {
+            if (i > 1)
+                result += ', ';
+
+            result += _generate(i);
+        }
+
+        return result;
+    };
+
+    var _generate = function (i) {
+        if (i % 3 == 0 && i % 5 == 0)
+            return 'FizzBuzz';
+        else if (i % 3 == 0)
+            return 'Fizz';
+        else if (i % 5 == 0)
+            return 'Buzz';
+        else
+            return i;
+    };
+
+    return function () {
+        return {
+            Print: _print
+        };
+    };
+})();
---
+++ MvcCrud/wwwroot/js/FizzBuzz.spec.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="FizzBuzz.js" />
+
+describe("FizzBuzz", function () {
+    it("Should Print Text For Values That Are Multiples Of 3 And 5", function () {
+        var fizzBuzzGenerator = new FizzBuzz();
+        var result = fizzBuzzGenerator.Print(15);
+        expect(result).toBe("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz", result);
+    });
+});

9e7abbd0c6efda0eeac8df9282d99440e55f6ed6
NJS <24832578+codenjs@users.noreply.github.com>
2019-07-19 23:18:28 -0400

    Override default login page

---
+++ MvcCrud/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MvcCrud.Data;
+
+[assembly: HostingStartup(typeof(MvcCrud.Areas.Identity.IdentityHostingStartup))]
+namespace MvcCrud.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

bc0df838c5b8143aff6cbfc06b3d9afa10b3779a
NJS <24832578+codenjs@users.noreply.github.com>
2019-07-20 13:48:47 -0400

    Create a Movie model and generate scaffolded controller and views for CRUD functionality

--- MvcCrud/Data/ApplicationDbContext.cs
+++ MvcCrud/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MvcCrud.Models;
 
 namespace MvcCrud.Data
 {
@@ -12,5 +13,6 @@ namespace MvcCrud.Data
             : base(options)
         {
         }
+        public DbSet<Movie> Movies { get; set; }
     }
 }

c06dc808b99128c9154698fe387d96d5c15fdba6
NJS <24832578+codenjs@users.noreply.github.com>
2019-07-20 14:31:53 -0400

    Add basic validation for movie titles

--- MvcCrud/Models/Movie.cs
+++ MvcCrud/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,9 @@ namespace MvcCrud.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "Title can't exceed {1} characters")]
         public string Title { get; set; }
     }
 }
